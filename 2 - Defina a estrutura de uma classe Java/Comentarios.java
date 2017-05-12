1a65d532d6b148c5d6827246b7f618b40fe5c1d6
George Salu <george.salu10@gmail.com>
2017-05-11 22:42:56 -0300

    estrutura de classe

---
+++ 2 - Defina a estrutura de uma classe Java/Comentarios.java
@@ -0,0 +1,16 @@
+//comentarios de linha
+/*
+    comentarios de 
+    multiplas linhas
+ */
+class /* comentarios no meio de linha */ Comentarios {
+
+    /**
+        JavaDoc, prepare que a primeira linha do comentario tem
+        2 asteriscos
+     */
+    public void metodo(){
+
+    }
+
+}
---
+++ 2 - Defina a estrutura de uma classe Java/x.java
@@ -0,0 +1,10 @@
+class x {
+    int j = -100;
+
+    x(int i){
+        if(i > 1)
+            return;
+        j = i;
+    }
+
+}
