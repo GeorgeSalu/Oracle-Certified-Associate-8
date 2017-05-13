a01aead4b16cb2747f69cfb198a989c568675cfc
George Salu <george.salu10@gmail.com>
2017-05-13 15:53:22 -0300

    java executaveis

---
+++ 1 - O basico de java/Crie aplicações Java executáveis com um método main/Main.java
@@ -0,0 +1,18 @@
+class Main {
+
+    // parametro como array
+    public static void main(String[] args) {}
+
+    // parametro como varags
+    public static void main(String... args){}
+
+    // A ordem dos modificadores nao importa
+    static public void main(String[] args) {}
+
+    // o nome do paramentro nao importa
+    public static void main(String... argumentos) {}
+
+    // tambem é uma definicao valida de array
+    public static void main(String args[]) {}
+
+}
