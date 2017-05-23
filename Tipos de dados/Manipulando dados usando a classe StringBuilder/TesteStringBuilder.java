138e035733b7e0516b8ad9669bc8492b472059f0
George Salu <george.salu10@gmail.com>
2017-05-22 22:12:14 -0300

    iniciando Stringbuilders

--- Tipos de dados/Manipulando dados usando a classe StringBuilder/TesteStringBuilder.java
+++ Tipos de dados/Manipulando dados usando a classe StringBuilder/TesteStringBuilder.java
@@ -6,5 +6,17 @@ class TesteStringBuilder {
         sb.append(" - ");
         sb.append("Ensino e Inovacao");
 
+        //caelum - Ensino e inovacao
+        System.out.println(sb);
+
+        //vazio
+        StringBuilder sb1 = new StringBuilder();
+        //conteudo inicial
+        StringBuilder sb2 = new StringBuilder("java");
+        //tamanho inicial do array para colocar a string
+        StringBuilder sb3 = new StringBuilder(50);
+        //baseado em outro objeto do mesmo tipo
+        StringBuilder sb4 = new StringBuilder(sb2)
+
     }
 }
