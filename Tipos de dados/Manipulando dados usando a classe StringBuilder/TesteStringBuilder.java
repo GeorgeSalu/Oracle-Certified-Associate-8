aaf31993d06f5ef3f84426195d337ac5990a0992
George Salu <george.salu10@gmail.com>
2017-05-22 21:56:39 -0300

    manipulando string com stringbuilder

---
+++ Tipos de dados/Manipulando dados usando a classe StringBuilder/TesteStringBuilder.java
@@ -0,0 +1,10 @@
+class TesteStringBuilder {
+    public static void main(String args[]){
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Caelum");
+        sb.append(" - ");
+        sb.append("Ensino e Inovacao");
+
+    }
+}
