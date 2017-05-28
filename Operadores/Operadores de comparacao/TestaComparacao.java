8236b33e384adb3fe38abe41b60503a44e92dbad
George Salu <george.salu10@gmail.com>
2017-05-28 15:45:25 -0300

    operadores de comparacao

---
+++ Operadores/Operadores de comparacao/TestaComparacao.java
@@ -0,0 +1,17 @@
+class TestaComparacao {
+    public static void main(String args[]){
+
+        //operadores de comparacao
+        boolean mentira = false;
+        System.out.println(mentira == true);
+
+        System.out.println(true == true);
+        System.out.println(true != false);
+
+        System.out.println(1.0f == 1.0d);
+        System.out.println(1.0f == 1l);
+
+        System.out.println(1 == 1.0);
+
+    }
+}
