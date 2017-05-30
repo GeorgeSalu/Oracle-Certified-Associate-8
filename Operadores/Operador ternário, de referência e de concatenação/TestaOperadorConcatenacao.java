3a4e88b61ff46ec2fd90f9566ca99519b57acdca
George Salu <george.salu10@gmail.com>
2017-05-29 22:21:48 -0300

    operador de referencia,ternario e de concatenacao

---
+++ Operadores/Operador ternário, de referência e de concatenação/TestaOperadorConcatenacao.java
@@ -0,0 +1,6 @@
+class TestaOperadorConcatenacao {
+    public static void main(String [] args){
+        String nome = "Certificacao "+" Java";
+        
+    }
+}
---
+++ Operadores/Operador ternário, de referência e de concatenação/TestaOperadorDeReferencia.java
@@ -0,0 +1,6 @@
+class TestaOperadorDeReferencia {
+    public static void main(String [] args){
+        String nome = "George";
+        int tamanho = nome.length();
+    }
+}
