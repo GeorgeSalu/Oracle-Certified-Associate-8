2c8ce7507ca2752f0d07c024842e32ab724698d6
George Salu <george.salu10@gmail.com>
2017-06-03 15:42:13 -0300

    uso if e if-else

---
+++ Operadores/Utilize o if e ifelse/TesteIfeIfElse.java
@@ -0,0 +1,21 @@
+class TesteIfeIfElse {
+
+    boolean autentico = true;
+    if(autentico){
+        System.out.println("Usuario aceito");
+    }else{
+        System.out.println("Usuario incorreto");
+    }
+
+    //condicoes
+    // erro, numero inteiro
+    if(1 -2){
+
+    }
+
+    //ok, resultado boolean
+    if(1 < 2){
+
+    }
+
+}
