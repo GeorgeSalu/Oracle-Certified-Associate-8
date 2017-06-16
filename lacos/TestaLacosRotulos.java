fbbd50014be5e0936433751d3ef2ab8e72f4ae46
George Salu <george.salu10@gmail.com>
2017-06-15 22:01:53 -0300

    lacos - rotulos

--- lacos/TestaLacosRotulos.java
+++ lacos/TestaLacosRotulos.java
@@ -1,6 +1,10 @@
 class TestaLacosRotulos {
     public static void main(String [] args){
 
+
+        /**
+            labels statements
+         */
         externo: for(int i = 1;i < 10;i++){
             interno: for(int j = 1;j < 10;j++ ){
                 if(i * j == 25){
