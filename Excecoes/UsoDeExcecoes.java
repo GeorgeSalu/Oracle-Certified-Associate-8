38f847903318737a1ee0a7c92ec1cdeb91f2fa6c
George Salu <george.salu10@gmail.com>
2017-06-30 14:04:29 -0300

    o que sao excecoes e para que sao usadas

---
+++ Excecoes/UsoDeExcecoes.java
@@ -0,0 +1,7 @@
+public void fazAlgo(int[] idades) {
+    if(idades[0]==null) return; 
+    // return para caso o Java devolva nulo ao acessar 
+    // uma posição inválida
+
+    System.out.println(idades[0]);
+}
